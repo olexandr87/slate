963d24c83546ae74d1d3d828cf7f33004c9d06b1
unknown <daniel.salvado@tradingeconomics.com>
2017-02-07 12:22:34 +0000

    testing slate

--- config.rb
+++ config.rb
@@ -14,7 +14,7 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'fonts'
-
+set :navbar_url, 'http://www.tradingeconomics.com/'
 # Activate the syntax highlighter
 activate :syntax
 
